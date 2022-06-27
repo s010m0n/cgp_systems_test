edd969d96c16efe664586623897ef23afbf7a7e6
olegsolovjn@gmail.com <vodugu46>
2022-06-27 17:17:26 +0300

    start

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'You cannot sign with those credentials',
+                'errors' => 'Unauthorised'
+            ], 401);
+        }
+
+        $token = Auth::user()->createToken(config('app.name'));
+        $token->token->expires_at = $request->remember_me ?
+            Carbon::now()->addMonth() :
+            Carbon::now()->addDay();
+
+        $token->token->save();
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'token' => $token->accessToken,
+            'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString()
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create(array_merge(
+            $request->only('name', 'email'),
+            ['password' => bcrypt($request->password)],
+        ));
+
+        return response()->json([
+            'message' => 'You were successfully registered. Use your email and password to sign in.'
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'You are successfully logged out',
+        ]);
+    }
+}
