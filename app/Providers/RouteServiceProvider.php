6c1bca9459c8c15eed8a38c00d76a935136cbeb6
olegsolovjn@gmail.com <vodugu46>
2022-06-28 12:58:16 +0300

    home page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin/clients';
+    public const HOME = '/admin/companies';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
